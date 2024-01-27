3851ac5b127a481e63302f46351f60074bc80618
ShimaaElkadeem <shimaaelkadeem@gmaiL.com>
2024-01-27 18:21:36 +0200

    init project. added empty home screen.

--- lib/main.dart
+++ lib/main.dart
@@ -1,1 +1,21 @@
-void main() {}
+import 'package:deaf_call_app/home_screen.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      initialRoute: HomeScreen.routeName,
+      routes: {
+        HomeScreen.routeName: (context) => HomeScreen(),
+      },
+    );
+  }
+}
